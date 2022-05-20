eac65c9b361fde1c22502ea0df406428782b8158
qwerewqwerew <59301948+qwerewqwerew@users.noreply.github.com>
2022-05-20 11:46:10 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useRef, useState } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const App = () => {
+  const [data, setData] = useState([]);
+  let dataId = useRef(0);
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().toLocaleString();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+    console.log([newItem]);
+    console.log([...data]);
+  };
+
+  const onDelete = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  return (
+    <div className="App">
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onDelete={onDelete} diaryList={data} />
+    </div>
+  );
+};
+export default App;
